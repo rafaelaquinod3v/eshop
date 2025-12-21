78f1a10d584ae4de08ed69acf644b46bf33c08bd
Rafael Aquino <rafael.aquinoquijano@gmail.com>
2025-12-21 10:40:02 -0600

    home controller added

---
+++ apps/eshop-api/src/main/java/sv/com/eshop/HomeController.java
@@ -0,0 +1,13 @@
+package sv.com.eshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello my %s!", name);
+    }
+}
