87dd7a1552de281a48a52ebd5142e2930554f13f
Rafael Aquino <rafael.aquinoquijano@gmail.com>
2025-12-20 21:01:27 -0600

    initial commit

---
+++ apps/eshop-api/src/test/java/sv/com/eshop/EshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.com.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
