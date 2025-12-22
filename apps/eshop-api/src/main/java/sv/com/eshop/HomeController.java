8388d4ac2f50cfdcbadd963b4bfb36ec0adacf6a
Rafael Aquino <rafael.aquinoquijano@gmail.com>
2025-12-21 19:53:59 -0600

    pure java eshop-application-core added

--- apps/eshop-api/src/main/java/sv/com/eshop/HomeController.java
+++ apps/eshop-api/src/main/java/sv/com/eshop/HomeController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import sv.com.eshop.core.entities.Order;
+
 @RestController
 public class HomeController {
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+      return String.format("Hello %s! %d", name, new Order(12).id);
     }
 }
---
+++ apps/eshop-application-core/src/main/java/sv/com/eshop/core/entities/Order.java
@@ -0,0 +1,12 @@
+package sv.com.eshop.core.entities;
+public class Order{
+    public int id;
+    public Order(int id){
+        this.id = id;
+    }
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+}
